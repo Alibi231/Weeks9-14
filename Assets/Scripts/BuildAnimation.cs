7235aa06da352c5b0cceb5fbbcd939ee9c422c23
Alibi231 <hammond.michaeldz@gmail.com>
2026-03-17 10:44:42 -0400

    Finished setp 3 of Strategy Builder

--- Assets/Scripts/BuildAnimation.cs
+++ Assets/Scripts/BuildAnimation.cs
@@ -7,12 +7,12 @@ public class BuildAnimation : MonoBehaviour
     public GameObject armor;
     public GameObject repair;
     public AnimationCurve curve;
+    public float buildTime;
+    private Coroutine buildCoroutine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(growBuilding(ammo));
-        StartCoroutine(growBuilding(armor));
-        StartCoroutine(growBuilding(repair));
+        StartCoroutine(startBuilding());
     }
 
     // Update is called once per frame
@@ -21,14 +21,26 @@ public class BuildAnimation : MonoBehaviour
         
     }
 
+    IEnumerator startBuilding()
+    {
+        StartCoroutine(growBuilding(ammo));
+        yield return new WaitForSeconds(buildTime);
+
+        StartCoroutine(growBuilding(armor));
+        yield return new WaitForSeconds(buildTime);
+
+        StartCoroutine(growBuilding(repair));
+    }
+
     IEnumerator growBuilding(GameObject building)
     {
         float t = 0;
         building.transform.localScale = Vector3.zero;
 
-        while (t < 1)
+        while (t < buildTime)
         {
             t += Time.deltaTime;
+            Debug.Log(t);
             building.transform.localScale = Vector3.one * curve.Evaluate(t);
             yield return null;
         }
