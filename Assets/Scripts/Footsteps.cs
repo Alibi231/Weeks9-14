774f5d70cd57d3950e913615a37e91afc096153b
hammonmi <hammonmi@OA-S141-12>
2026-03-24 10:43:31 -0400

    Finished Coding Gym Week 11 "Footsteps".

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     public AudioSource footstep;
+    public AudioClip[] footstepClips;
+    public ParticleSystem dust;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +19,8 @@ public class Footsteps : MonoBehaviour
 
     public void Footstep()
     {
-        footstep.Play();
-        Debug.Log("WWW");
+        footstep.PlayOneShot(footstepClips[Random.Range(0, footstepClips.Length)]);
+        dust.Emit(Random.Range(15, 40));
+
     }
 }
