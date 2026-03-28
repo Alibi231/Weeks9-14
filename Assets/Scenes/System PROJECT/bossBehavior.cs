7e938f0addb5b788521b0c42e704838e3db367fc
Alibi231 <hammond.michaeldz@gmail.com>
2026-03-27 23:22:50 -0400

    Made boss behavior work using a coroutine system

--- Assets/Scenes/System PROJECT/bossBehavior.cs
+++ Assets/Scenes/System PROJECT/bossBehavior.cs
@@ -7,11 +7,24 @@ public class bossBehavior : MonoBehaviour
     //boss will have 3 attacks to cycle between, that are chosen randomly.
     public int behavior = 1;
     public Coroutine currentAction = null;
+    public float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        behavior = Random.Range(1, 4);
+        switch (behavior)
+        {
+            case 1:
+                currentAction = StartCoroutine(Attack1());
+                break;
+            case 2:
+                currentAction = StartCoroutine(Attack2());
+                break;
+            case 3:
+                currentAction = StartCoroutine(Attack3());
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -26,10 +39,10 @@ public class bossBehavior : MonoBehaviour
                     currentAction = StartCoroutine(Attack1());
                     break;
                 case 2:
-                    print("Attack 2");
+                    currentAction = StartCoroutine(Attack2());
                     break;
                 case 3:
-                    print("Attack 3");
+                    currentAction = StartCoroutine(Attack3());
                     break;
             }
         }
@@ -37,7 +50,63 @@ public class bossBehavior : MonoBehaviour
 
     IEnumerator Attack1()
     {
-        yield return null;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+
+        }
+        //Instantiate Fireball
+        while (timer < 6)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+
+        }
+        StopCoroutine(currentAction);
+        timer = 0;
+        currentAction = null;
+
+    }
+
+    IEnumerator Attack2()
+    {
+        while (timer < 3)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+
+        }
+        //Instantiate Explosion
+        while (timer < 6)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+
+        }
+        StopCoroutine(currentAction);
+        timer = 0;
+        currentAction = null;
+    }
+
+    IEnumerator Attack3()
+    {
+        while (timer < 1)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+
+        }
+        //Instantiate Multiple Spikes
+        while (timer < 3)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+
+        }
+        StopCoroutine(currentAction);
+        timer = 0;
+        currentAction = null;
     }
 
 }
